de36387c7dcf9f9d53105b9d890951b679eeeaaa
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-03-05 00:18:30 +0100

    Update morphology-erosion-dilation.py

--- 3a_Morphology/morphology-erosion-dilation.py
+++ 3a_Morphology/morphology-erosion-dilation.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from skimage.morphology import erosion, dilation
 
 # Open the image using PIL
-image = Image.open("resources/gears.jpeg").convert("L")
+image = Image.open("path-to-resources/gears.jpeg").convert("L")
 
 # Convert image to a NumPy array
 data = np.array(image, dtype=np.uint8)

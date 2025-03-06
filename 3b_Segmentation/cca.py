7d38668747228f50aaccc2472155fcb9251f7d78
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-03-06 23:40:19 +0100

    Update cca.py

--- 3b_Segmentation/cca.py
+++ 3b_Segmentation/cca.py
@@ -1,47 +1,26 @@
+from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
-from skimage import data
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
+from skimage.measure import label
 from skimage.color import label2rgb
 
+# Open the image using PIL
+image = Image.open("path-to-resources/ovals.jpeg").convert("L")
 
-image = data.coins()[50:-50, 50:-50]
+# Convert image to a NumPy array
+data = np.array(image, dtype=np.uint8)
 
-# apply threshold
-thresh = threshold_otsu(image)
-bw = closing(image > thresh, square(3))
+# Apply thresholding
+threshold = 128
+data = np.where(data > threshold, 0, 1)
 
-# remove artifacts connected to image border
-cleared = clear_border(bw)
+# Label connected regions (CCA)
+labels_from_image = label(data)
+# Produce an image where labels are color-coded
+image_label_overlay = label2rgb(labels_from_image, image=data, alpha=1.0)
 
-# label image regions
-label_image = label(cleared)
-# to make the background transparent, pass the value of `bg_label`,
-# and leave `bg_color` as `None` and `kind` as `overlay`
-image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
-
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.imshow(image_label_overlay)
-
-for region in regionprops(label_image):
-    # take regions with large enough areas
-    if region.area >= 100:
-        # draw rectangle around segmented coins
-        minr, minc, maxr, maxc = region.bbox
-        rect = mpatches.Rectangle(
-            (minc, minr),
-            maxc - minc,
-            maxr - minr,
-            fill=False,
-            edgecolor='red',
-            linewidth=2,
-        )
-        ax.add_patch(rect)
-
-ax.set_axis_off()
-plt.tight_layout()
+# Display the image
+plt.imshow(image_label_overlay)
+plt.title("Center point")
+plt.axis('off')
 plt.show()

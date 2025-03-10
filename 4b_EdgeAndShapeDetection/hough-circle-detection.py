a8625f5d252ce12dbba0bd8482c5a135d58f7a8e
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-03-10 14:46:19 +0100

    Update hough-circle-detection.py

--- 4b_EdgeAndShapeDetection/hough-circle-detection.py
+++ 4b_EdgeAndShapeDetection/hough-circle-detection.py
@@ -2,10 +2,14 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.feature import canny
-from skimage.transform import hough_line, hough_line_peaks
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.draw import circle_perimeter
 
 # Open the image using PIL
-image = Image.open("resources/cone.jpg").convert("L")
+# L. Antonelli, F. Polverino, A. Albu, A. Hada, I.A. Asteriti, F. Degrassi, G. Guarguaglini, L. Maddalena, M.R. Guarracino, 
+# ALFI: Cell cycle phenotype annotations of label-free time-lapse imaging data from cultured human cells, Scientific Data 10(677), 2023
+# (Image: I_TP03_0014)
+image = Image.open("resources/cells.jpg").convert("L")
 
 # Convert image to a NumPy array
 data = np.array(image, dtype = np.uint8)
@@ -13,23 +17,26 @@ data = np.array(image, dtype = np.uint8)
 # Compute the edges using the Canny filter
 edges = canny(data, sigma = 0.5)
 
-# Create test angles and compute their Hough transform
-test_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
-hspace, theta, distances = hough_line(edges, theta = test_angles)
+# Create test circles and compute their Hough transform
+test_rs = np.arange(40, 60, 2)
+hough_result = hough_circle(edges, test_rs)
+accums, cx, cy, rs = hough_circle_peaks(hough_result, test_rs, total_num_peaks = 10)
+
+# Empty image to draw circles into
+final_image = np.zeros_like(data)
+
+# Draw the circles
+for center_y, center_x, radius in zip(cy, cx, rs):
+    circy, circx = circle_perimeter(center_y, center_x, radius, shape=data.shape)
+    final_image[circy, circx] = 255
 
 # Display images
 fig, ax = plt.subplots(1, 2, figsize=(12, 6))
 ax[0].imshow(data, cmap = "gray")
-ax[0].set_title('Original image')
+ax[0].set_title("Original Image")
 ax[0].axis("off")
-ax[1].imshow(data, cmap = "gray")
-ax[1].set_ylim((data.shape[0], 0))
-ax[1].set_xlim((0, data.shape[1]))
-ax[1].set_title('Detected Lines')
+ax[1].imshow(final_image, cmap = "gray")
+ax[1].set_title("Detected Circles")
 ax[1].axis("off")
-# Draw lines
-for _, angle, dist in zip(*hough_line_peaks(hspace, theta, distances, threshold = 0.2 * np.max(hspace))):
-    (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
-    ax[1].axline((x0, y0), slope=np.tan(angle + np.pi / 2))
 plt.tight_layout()
 plt.show()

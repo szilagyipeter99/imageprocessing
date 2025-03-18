2a0594cef1af3f9eb3ac3acd928ad4d661f92b9e
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-03-19 00:00:00 +0100

    Update compare-denoising-methods.py

--- 4a_NoiseReduction/compare-denoising-methods.py
+++ 4a_NoiseReduction/compare-denoising-methods.py
@@ -25,24 +25,18 @@ denoised_image_wl = denoise_wavelet(data)
 denoised_image_nlm = denoise_nl_means(data, h = 0.1, fast_mode = "True", patch_size = 5, patch_distance = 6)
 
 # Display images
-fig, ax = plt.subplots(2, 3, figsize = (12, 8))
-ax[0][0].imshow(data, cmap = "gray")
-ax[0][0].set_title("Original Image")
-ax[0][0].axis("off")
-ax[0][1].imshow(denoised_image_med, cmap = "gray")
-ax[0][1].set_title("Median filter")
-ax[0][1].axis("off")
-ax[0][2].imshow(denoised_image_bl, cmap = "gray")
-ax[0][2].set_title("Bilateral filter")
-ax[0][2].axis("off")
-ax[1][0].imshow(denoised_image_tv, cmap = "gray")
-ax[1][0].set_title("Total variation")
-ax[1][0].axis("off")
-ax[1][1].imshow(denoised_image_wl, cmap = "gray")
-ax[1][1].set_title("Wavelet")
-ax[1][1].axis("off")
-ax[1][2].imshow(denoised_image_nlm, cmap = "gray")
-ax[1][2].set_title("Non-local means")
-ax[1][2].axis("off")
+filters = [
+    ("Original Image", data),
+    ("Median filter", denoised_image_med),
+    ("Bilateral filter", denoised_image_bl),
+    ("Total variation", denoised_image_tv),
+    ("Wavelet", denoised_image_wl),
+    ("Non-local means", denoised_image_nlm)
+]
+fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+for ax, (title, img) in zip(axes.ravel(), filters):
+    ax.imshow(img, cmap="gray")
+    ax.set_title(title)
+    ax.axis("off")
 plt.tight_layout()
 plt.show()

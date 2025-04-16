3fa3393ca453ab7d8a606d747fa8e593cd69a65d
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-04-16 13:13:28 +0200

    Update mobilenetv3-lapa-finetuning.py

--- 7_TransferLearning/mobilenetv3-lapa-finetuning.py
+++ 7_TransferLearning/mobilenetv3-lapa-finetuning.py
@@ -4,22 +4,23 @@ from keras import layers, applications, Model
 from PIL import Image
 import numpy as np 
 
-dataset_folder = 'path-to-resources/LaPa'
+dataset_folder = 'resources/LaPa'
 
 def preprocess_image(img_path):
     img = Image.open(img_path).convert("RGB")
-    img = img.resize((224, 224))
+    img_h, img_w = img.size[1], img.size[0] 
+    img = img.resize((256, 256))
     img_data = np.array(img, dtype=np.uint8)
     img_data = img_data / 255.0
-    return img_data
+    return img_data, img_h, img_w
 
-def load_landmarks(txt_path):
+def load_landmarks(txt_path, img_h, img_w):
     with open(txt_path, "r") as f:
         lines = f.readlines()[1:]
         coords = [list(map(float, line.strip().split())) for line in lines]
         coords = np.array(coords)
-        coords[:, 0] = coords[:, 0] / 224
-        coords[:, 1] = coords[:, 1] / 224
+        coords[:, 0] = coords[:, 0] / img_w
+        coords[:, 1] = coords[:, 1] / img_h
         return coords.flatten().astype(np.float32)
 
 
@@ -30,19 +31,19 @@ def create_dataset(image_dir, txt_dir):
             img_path = os.path.join(image_dir, fname)
             txt_name = os.path.splitext(fname)[0] + ".txt"
             txt_path = os.path.join(txt_dir, txt_name)
-            img_data = preprocess_image(img_path)
-            landmarks = load_landmarks(txt_path)
+            img_data, img_h, img_w = preprocess_image(img_path)
+            landmarks = load_landmarks(txt_path, img_h, img_w)
             yield img_data.astype(np.float32), landmarks
     ds = tf.data.Dataset.from_generator(
         gen,
         output_signature=(
-            tf.TensorSpec(shape=(224, 224, 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(256, 256, 3), dtype=tf.float32),
             tf.TensorSpec(shape=(212,), dtype=tf.float32)
         )
     )
     return ds.shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
 
-inputs = layers.Input(shape=(224, 224, 3))
+inputs = layers.Input(shape=(256, 256, 3))
 backbone = applications.MobileNetV3Small(include_top=False, weights="imagenet", input_tensor=inputs)
 backbone.trainable = True
 x = backbone.output
@@ -56,6 +57,6 @@ model.summary()
 train_ds = create_dataset(f"{dataset_folder}/train/images", f"{dataset_folder}/train/landmarks").repeat()
 val_ds = create_dataset(f"{dataset_folder}/val/images", f"{dataset_folder}/val/landmarks").repeat()
 
-model.fit(train_ds, validation_data=val_ds, steps_per_epoch=568, validation_steps=63, epochs=25)
+model.fit(train_ds, validation_data=val_ds, steps_per_epoch=568, validation_steps=63, epochs=5)
 
 model.save('mobilenetv3_lapa.keras')

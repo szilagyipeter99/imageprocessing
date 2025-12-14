07c1bcf8c415412227a5bf9de8ac691c3a44d861
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-14 23:07:39 +0100

    Refactor AlexNet model for CIFAR-10 classification

--- 6_MulticlassClassification/cifar10-alexnet.py
+++ 6_MulticlassClassification/cifar10-alexnet.py
@@ -11,33 +11,31 @@ x_train, x_val = x_train / 255.0, x_val / 255.0
 y_train = utils.to_categorical(y_train, 10)
 y_val = utils.to_categorical(y_val, 10)
 
-# Data Augmentation (AlexNet used heavy augmentation)
+# Data Augmentation
 data_augmentation = Sequential([
     layers.RandomFlip("horizontal"),
     layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1)
+    layers.RandomZoom(0.1),
+    layers.RandomTranslation(0.1, 0.1)
 ])
 
-# AlexNet-inspired model
 model = Sequential([
-    layers.Input(shape=(32, 32, 3)), # CIFAR-10 is 32x32, AlexNet was 227x227
+    layers.Input(shape=(32, 32, 3)), # CIFAR-10 images are 32x32
     data_augmentation,
-    layers.Conv2D(96, kernel_size = (3, 3), strides = 1, activation = 'relu', padding = 'same'), # Original AlexNet: (11x11, stride 4)
-    layers.BatchNormalization(), # Alexnet used Local Response Normalization (LRN), Batch Normalization (BN) was introduced in 2015 by Sergey Ioffe and Christian Szegedy
-    layers.MaxPooling2D(pool_size = (2, 2), strides = 2), # Original AlexNet: (3x3, stride 2)
-    layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation='relu'), # Original AlexNet: (5x5)
+    layers.Conv2D(96, kernel_size = (3, 3), activation = 'relu'),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
+    layers.Conv2D(256, kernel_size = (3, 3), activation='relu'),
     layers.BatchNormalization(),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
     layers.Conv2D(384, kernel_size = (3, 3), padding='same', activation='relu'),
     layers.Conv2D(384, kernel_size = (3, 3), padding='same', activation='relu'),
     layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation='relu'),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
-    layers.Flatten(),
-    layers.Dense(1024, activation='relu'), # Original AlexNet: 4096
+    layers.GlobalAveragePooling2D(),
+    layers.Dense(512, activation='relu'),
     layers.Dropout(0.5),
-    layers.Dense(1024, activation='relu'), # Original AlexNet: 4096
-    layers.Dropout(0.5),
-    layers.Dense(10, activation='softmax') # CIFAR-10 has 10 classes, AlexNet had 1000
+    layers.Dense(10, activation='softmax') # CIFAR-10 has 10 classes
 ])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()

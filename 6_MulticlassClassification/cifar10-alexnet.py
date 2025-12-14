aaaa5c35b48461dda66e2e517489db511f02c03b
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-14 23:14:53 +0100

    Update cifar10-alexnet.py

--- 6_MulticlassClassification/cifar10-alexnet.py
+++ 6_MulticlassClassification/cifar10-alexnet.py
@@ -22,17 +22,17 @@ data_augmentation = Sequential([
 model = Sequential([
     layers.Input(shape=(32, 32, 3)), # CIFAR-10 images are 32x32
     data_augmentation,
-    layers.Conv2D(96, kernel_size = (3, 3), padding='same', activation = 'none'),
+    layers.Conv2D(96, kernel_size = (3, 3), padding='same', activation = None),
     layers.BatchNormalization(),
     layers.Activation('relu'),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
-    layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation='none'),
+    layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation = None),
     layers.BatchNormalization(),
     layers.Activation('relu'),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
-    layers.Conv2D(384, kernel_size = (3, 3), padding='same', activation='relu'),
-    layers.Conv2D(384, kernel_size = (3, 3), padding='same', activation='relu'),
-    layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation='relu'),
+    layers.Conv2D(384, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
+    layers.Conv2D(384, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
+    layers.Conv2D(256, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
     layers.GlobalAveragePooling2D(),
     layers.Dense(512, activation='relu'),
@@ -43,7 +43,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 model.summary()
 
 # Train the model
-history = model.fit(x_train, y_train, epochs = 30, batch_size = 128, validation_data=(x_val, y_val))
+history = model.fit(x_train, y_train, epochs = 30, batch_size = 128, validation_data = (x_val, y_val))
 
 # Evaluate the trained model
 score = model.evaluate(x_val, y_val, verbose=0)

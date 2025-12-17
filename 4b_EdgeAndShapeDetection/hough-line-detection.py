6dbf720026e40f09fe13920fdbaf3f257726bce5
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-17 18:55:23 +0100

    Format code for consistency in hough-line-detection.py

--- 4b_EdgeAndShapeDetection/hough-line-detection.py
+++ 4b_EdgeAndShapeDetection/hough-line-detection.py
@@ -14,11 +14,11 @@ data = np.array(image, dtype = np.uint8)
 edges = canny(data, sigma = 0.5)
 
 # Create test angles and compute their Hough transform
-test_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
+test_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint = False)
 hspace, theta, distances = hough_line(edges, theta = test_angles)
 
 # Display images
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig, ax = plt.subplots(1, 2, figsize = (12, 6))
 ax[0].imshow(data, cmap = "gray")
 ax[0].set_title('Original image')
 ax[0].axis("off")
@@ -30,6 +30,6 @@ ax[1].axis("off")
 # Draw lines
 for _, angle, dist in zip(*hough_line_peaks(hspace, theta, distances, threshold = 0.2 * np.max(hspace))):
     (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
-    ax[1].axline((x0, y0), slope=np.tan(angle + np.pi / 2))
+    ax[1].axline((x0, y0), slope = np.tan(angle + np.pi / 2))
 plt.tight_layout()
 plt.show()

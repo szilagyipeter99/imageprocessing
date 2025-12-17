b50bfb333de34a622108e1dd311adc4f84dfe326
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-17 18:48:55 +0100

    Update cca.py

--- 3b_Segmentation/cca.py
+++ 3b_Segmentation/cca.py
@@ -8,7 +8,7 @@ from skimage.color import label2rgb
 image = Image.open("path-to-resources/ovals.jpeg").convert("L")
 
 # Convert image to a NumPy array
-data = np.array(image, dtype=np.uint8)
+data = np.array(image, dtype = np.uint8)
 
 # Apply thresholding
 threshold = 128
@@ -17,7 +17,7 @@ data = np.where(data > threshold, 0, 1)
 # Label connected regions (CCA)
 labels_from_image = label(data)
 # Produce an image where labels are color-coded
-image_label_overlay = label2rgb(labels_from_image, image=data, alpha=1.0)
+image_label_overlay = label2rgb(labels_from_image, image = data, alpha = 1.0)
 
 # Display the image
 plt.imshow(image_label_overlay)

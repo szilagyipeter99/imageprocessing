1778ffa3cdccb07a5a8c69ebafc83269f7980ae3
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-17 18:49:26 +0100

    Format code for consistency in k-means.py

--- 3b_Segmentation/k-means.py
+++ 3b_Segmentation/k-means.py
@@ -7,7 +7,7 @@ from sklearn.cluster import KMeans
 image = Image.open("path-to-resources/cone.jpg")
 
 # Convert image to a NumPy array
-data = np.array(image, dtype=np.uint8)
+data = np.array(image, dtype = np.uint8)
 
 # Get the dimensions of the image
 image_h, image_w, _ = data.shape
@@ -22,7 +22,7 @@ kmeans.fit(pixels)
 labels = np.reshape(kmeans.labels_, (image_h, image_w))
 
 # Display images
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig, ax = plt.subplots(1, 2, figsize = (12, 6))
 ax[0].imshow(data)
 ax[0].set_title("Original Image")
 ax[0].axis("off")

5c4974042f354fcf2e827c5643090d0e91edcc7a
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-17 18:51:36 +0100

    Format code for consistency in 2d-convolution.py

--- 3c_Convolution/2d-convolution.py
+++ 3c_Convolution/2d-convolution.py
@@ -7,7 +7,7 @@ from scipy.signal import convolve2d
 image = Image.open("path-to-resources/boglarka-low.jpg")
 
 # Convert image to a NumPy array
-data = np.array(image, dtype=np.uint8)
+data = np.array(image, dtype = np.uint8)
 
 # Define a 5x5 kernel 
 kernel = np.array([[1, 1, 1, 1, 1, 1, 1],
@@ -24,10 +24,10 @@ kernel = kernel / kernel.sum()
 # Perform the 2D convolution on each color channel
 convolved_image_channels = [convolve2d(data[:, :, i], kernel) for i in range(3)]
 # Combine the three channels back into one array
-convolved_image = np.stack(convolved_image_channels, axis=-1).astype(np.uint8)
+convolved_image = np.stack(convolved_image_channels, axis = -1).astype(np.uint8)
 
 # Display images
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig, ax = plt.subplots(1, 2, figsize = (12, 6))
 ax[0].imshow(image)
 ax[0].set_title("Original Image")
 ax[0].axis("off")

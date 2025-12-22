4595d5a258c5575e4cfcbd572db924e946f8d233
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2025-12-22 13:53:02 +0100

    Update cifar10-cnn.py

--- 6_MulticlassClassification/cifar10-cnn.py
+++ 6_MulticlassClassification/cifar10-cnn.py
@@ -14,38 +14,36 @@ y_val = utils.to_categorical(y_val, 10)
 # Data Augmentation
 data_augmentation = Sequential([
     layers.RandomFlip("horizontal"),
-    layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1),
-    layers.RandomTranslation(0.1, 0.1)
+    layers.RandomZoom(0.05),
+    layers.RandomTranslation(0.05, 0.05)
 ])
 
 model = Sequential([
     layers.Input(shape=(32, 32, 3)), # CIFAR-10 images are 32x32
     data_augmentation,
-    layers.Conv2D(96, kernel_size = (3, 3), padding='same', activation = None),
+    layers.Conv2D(64, kernel_size = (3, 3), padding='same', activation = None),
+    layers.BatchNormalization(),
+    layers.Activation('relu'),
+    layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
+    layers.Conv2D(128, kernel_size = (3, 3), padding='same', activation = None),
     layers.BatchNormalization(),
     layers.Activation('relu'),
     layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
     layers.Conv2D(256, kernel_size = (3, 3), padding='same', activation = None),
     layers.BatchNormalization(),
     layers.Activation('relu'),
-    layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
-    layers.Conv2D(384, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
-    layers.Conv2D(384, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
-    layers.Conv2D(256, kernel_size = (3, 3), padding = 'same', activation = 'relu'),
-    layers.MaxPooling2D(pool_size = (2, 2), strides = 2),
     layers.GlobalAveragePooling2D(),
-    layers.Dense(512, activation='relu'),
-    layers.Dropout(0.5),
     layers.Dense(10, activation='softmax') # CIFAR-10 has 10 classes
 ])
 # Stochastic Gradient Descent optimizer
-optimizer = optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+# with learning rate scheduling
+scheduled_lr = optimizers.schedules.CosineDecay(initial_learning_rate=0.05, decay_steps = len(x_train) // 128 * 50)
+optimizer = optimizers.SGD(learning_rate=scheduled_lr, momentum=0.9, nesterov=True)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
 # Train the model
-history = model.fit(x_train, y_train, epochs = 30, batch_size = 128, validation_data = (x_val, y_val))
+history = model.fit(x_train, y_train, epochs = 50, batch_size = 128, validation_data = (x_val, y_val))
 
 # Evaluate the trained model
 score = model.evaluate(x_val, y_val, verbose=0)

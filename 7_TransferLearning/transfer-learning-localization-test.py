c472d2fcaf6a1bad3efea2f33635ec5b5a0a7b51
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2026-03-26 22:56:40 +0100

    Update transfer-learning-localization-test.py

--- 7_TransferLearning/transfer-learning-localization-test.py
+++ 7_TransferLearning/transfer-learning-localization-test.py
@@ -15,9 +15,9 @@ test_folder = "resources/dog-sequence"
 def preprocess_image(img_path):
     img = Image.open(img_path).convert("RGB")
     img = img.resize((224, 224))
-    img_data = np.array(img, dtype=np.uint8)
+    img_data = np.array(img, dtype = np.uint8)
     img_data = img_data / 255.0
-    img_data = np.expand_dims(img_data, axis=0)
+    img_data = np.expand_dims(img_data, axis = 0)
     return img, img_data
 
 test_image_paths = [os.path.join(test_folder, fname) for fname in os.listdir(test_folder) if fname.endswith((".jpg", ".jpeg", ".png", ".webp"))]
@@ -37,7 +37,7 @@ for file in sorted(test_image_paths):
     box_height = ymax - ymin
     ax.clear()
     ax.imshow(test_img)
-    rect = patches.Rectangle((xmin, ymin), box_width, box_height, linewidth=2, edgecolor = "red", facecolor = "none")
+    rect = patches.Rectangle((xmin, ymin), box_width, box_height, linewidth = 2, edgecolor = "red", facecolor = "none")
     ax.add_patch(rect)
     ax.axis("off")    
     plt.pause(.033)
@@ -56,7 +56,7 @@ for file in sorted(test_image_paths):
 #     box_height = ymax - ymin
 #     fig, ax = plt.subplots()
 #     ax.imshow(test_img)
-#     rect = patches.Rectangle((xmin, ymin), box_width, box_height, linewidth=2, edgecolor = "red", facecolor = "none")
+#     rect = patches.Rectangle((xmin, ymin), box_width, box_height, linewidth = 2, edgecolor = "red", facecolor = "none")
 #     ax.add_patch(rect)
 #     ax.axis("off")    
 #     plt.show()
